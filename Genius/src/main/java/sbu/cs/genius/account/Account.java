45cd6a159223d0e29677a7c86e79289da991c3d9
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-03-29 22:26:48 +0330

    completed Account.java

--- Genius/src/main/java/sbu/cs/genius/account/Account.java
+++ Genius/src/main/java/sbu/cs/genius/account/Account.java
@@ -1,4 +1,28 @@
 package sbu.cs.genius.account;
 
-public class Account {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Account {
+
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private int age;
+
+    public Account(String name, String username, String email, String password, int age) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public static boolean validPassword(String password) {
+        Pattern pattern = Pattern.compile("(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+    }
+
 }
