e1065e883add1088e4035bc7d3899d1ede21a5fc
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-03-29 22:31:09 +0330

    completed Artist.java

--- Genius/src/main/java/sbu/cs/genius/account/Artist.java
+++ Genius/src/main/java/sbu/cs/genius/account/Artist.java
@@ -1,4 +1,18 @@
 package sbu.cs.genius.account;
 
-public class Artist {
+public class Artist extends Account{
+
+    private static Artist artist;
+
+    public Artist (String name, String username, String email, String password, int age) {
+        super(name, username, email, password, age);
+    }
+
+    public static Artist getArtist() {
+        return artist;
+    }
+
+    public static void setArtist(Artist artist) {
+        Artist.artist = artist;
+    }
 }
