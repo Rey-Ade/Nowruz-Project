410d8e195d0c83741e0fe73bc8c5728c4da492e9
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-03-29 22:29:31 +0330

    completed User.java

--- Genius/src/main/java/sbu/cs/genius/account/User.java
+++ Genius/src/main/java/sbu/cs/genius/account/User.java
@@ -1,4 +1,18 @@
 package sbu.cs.genius.account;
 
-public class User {
+public class User extends Account{
+
+    private static User user;
+
+    public User(String name, String username, String email, String password, int age) {
+        super(name, username, email, password, age);
+    }
+
+    public static User getUser() {
+        return user;
+    }
+
+    public static void setUser(User user) {
+        User.user = user;
+    }
 }
