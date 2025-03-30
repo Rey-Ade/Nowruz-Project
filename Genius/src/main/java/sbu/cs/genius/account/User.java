ddd30aba785e4b3c8e38aa197e5edb8bc3e5fd63
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-03-30 17:46:42 +0330

    updated User.java

--- Genius/src/main/java/sbu/cs/genius/account/User.java
+++ Genius/src/main/java/sbu/cs/genius/account/User.java
@@ -1,8 +1,14 @@
 package sbu.cs.genius.account;
 
+import sbu.cs.genius.content.Song;
+
+import java.util.ArrayList;
+
 public class User extends Account{
 
     private static User user = null;
+
+    private ArrayList<Artist> followingList;
 
     public User(String name, String username, String email, String password, int age) {
         super(name, username, email, password, age);
@@ -14,5 +20,21 @@ public class User extends Account{
 
     public static void setUser(User user) {
         User.user = user;
+    }
+
+    public ArrayList<Artist> getFollowingList() {
+        return followingList;
+    }
+
+    public void addArtistToFollowingList(Artist artist) {
+        followingList.add(artist);
+    }
+
+    public void viewSong(Song song) {
+        song.updateViewsCount();
+    }
+
+    public void comment(Song song, String comment) {
+        song.addComment(comment);
     }
 }
