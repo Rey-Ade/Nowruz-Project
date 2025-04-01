913bf333da5925f6045433722f60dc51f3604c21
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-04-01 10:56:10 +0330

    updated home scene

--- Genius/src/main/java/sbu/cs/genius/HomeController.java
+++ Genius/src/main/java/sbu/cs/genius/HomeController.java
@@ -2,14 +2,21 @@ package sbu.cs.genius;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import sbu.cs.genius.account.Artist;
 import sbu.cs.genius.account.User;
 import sbu.cs.genius.content.Song;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -21,6 +28,13 @@ public class HomeController implements Initializable {
 
     @FXML
     private ListView<String> topSongsListView;
+
+    @FXML
+    private TextField searchTextField;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -69,5 +83,21 @@ public class HomeController implements Initializable {
 
     public void newSingle(ActionEvent event) {
         System.out.println("new single");
+    }
+
+    public void search(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sbu/cs/genius/search-view.fxml"));
+        root = loader.load();
+
+        // make an instance of SearchController class
+        SearchController searchController = loader.getController();
+        searchController.getSearchTerm(searchTextField.getText());
+
+        // switch to search scene
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
