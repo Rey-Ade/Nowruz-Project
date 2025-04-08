d8fd7794250ebf606811cef1e8dd321b213df0c7
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-04-08 03:43:41 +0330

    renamed artist-view and ArtistController

--- Genius/src/main/java/sbu/cs/genius/ContentController.java
+++ Genius/src/main/java/sbu/cs/genius/ContentController.java
@@ -4,12 +4,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import sbu.cs.genius.account.Artist;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ArtistController implements Initializable {
+public class ContentController implements Initializable {
 
     @FXML
     private Label heading, subheading, label1, label2, label3, label4, label5, content1, content2, content3, content4, content5;
