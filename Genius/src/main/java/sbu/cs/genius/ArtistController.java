03c9af438cefad92e0819b95da9df64cfef43736
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-04-11 19:34:40 +0330

    changing artist scene back to content scene

--- Genius/src/main/java/sbu/cs/genius/ArtistController.java
+++ Genius/src/main/java/sbu/cs/genius/ArtistController.java
@@ -10,10 +10,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import sbu.cs.genius.account.Artist;
 import sbu.cs.genius.content.Album;
+import sbu.cs.genius.content.Comment;
+
 import java.io.IOException;
+import java.util.Objects;
 
 public class ArtistController {
 
@@ -23,8 +27,11 @@ public class ArtistController {
     @FXML
     private ListView<String> recordsListView;
 
+    @FXML
+    AnchorPane pane;
 
-    public void setScene(Artist artist) {
+
+    public void setScene(Artist artist, ActionEvent event) {
         artistName.setText(artist.getName());
         artistUsername.setText(artist.getUsername());
         artistAge.setText(String.valueOf(artist.getAge()));
@@ -32,32 +39,33 @@ public class ArtistController {
         for (Album album : artist.getAlbumsByArtist()) {
             recordsListView.getItems().add(album.toString());
         }
-        System.out.println("-> artist scene is set");
 
-        recordsListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                int index = recordsListView.getSelectionModel().getSelectedIndex();
-                // load Album controller
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/sbu/cs/genius/album-view.fxml"));
-                Parent root = null;
-                try {
-                    root = loader.load();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-                AlbumController albumController = loader.getController();
-                albumController.setScene(artist.getAlbumsByArtist().get(index), "/sbu/cs/genius/artist-view.fxml");
-                // switch scenes
-                ActionEvent event = new ActionEvent();
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.setResizable(false);
-                stage.show();
-                System.out.println("-> switched to album scene");
-            }
-        });
+        System.out.println("-> artist scene is set");
+        pane.getChildren().add(getCommentLayout());
+
+//        recordsListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//                int index = recordsListView.getSelectionModel().getSelectedIndex();
+//                // load Album controller
+//                FXMLLoader loader = new FXMLLoader(getClass().getResource("/sbu/cs/genius/album-view.fxml"));
+//                Parent root = null;
+//                try {
+//                    root = loader.load();
+//                } catch (IOException ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//                AlbumController albumController = loader.getController();
+//                albumController.setScene(artist.getAlbumsByArtist().get(index), "/sbu/cs/genius/artist-view.fxml", event);
+//                // switch scenes
+//                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//                Scene scene = new Scene(root);
+//                stage.setScene(scene);
+//                stage.setResizable(false);
+//                stage.show();
+//                System.out.println("-> switched to album scene");
+//            }
+//        });
     }
 
     public void back(ActionEvent event) throws IOException {
@@ -70,5 +78,17 @@ public class ArtistController {
         stage.setResizable(false);
         stage.show();
         System.out.println("-> switch to browse scene");
+    }
+    
+    Node getCommentLayout() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sbu/cs/genius/comment-view.fxml"));
+        try {
+            Node node = loader.load();
+            node.setLayoutX(280);
+            node.setLayoutY(420);
+            return node;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
