807abd186e2974a024c973cfd7a677dbc95b0c96
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-04-13 00:30:36 +0330

    updated Comment, comment-view and CommentController

--- Genius/src/main/java/sbu/cs/genius/CommentController.java
+++ Genius/src/main/java/sbu/cs/genius/CommentController.java
@@ -1,4 +1,12 @@
 package sbu.cs.genius;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CommentController {
+
+    @FXML
+    public Label username, date;
+
+
 }
--- Genius/src/main/java/sbu/cs/genius/content/Comment.java
+++ Genius/src/main/java/sbu/cs/genius/content/Comment.java
@@ -1,4 +1,23 @@
 package sbu.cs.genius.content;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Comment {
+
+    private String date;
+    private String text;
+    private String username;
+
+    public Comment (String username, String text){
+        this.username = username;
+        date = formatDateTime(LocalDateTime.now());
+        this.text = text;
+    }
+
+    // format date and time and return a string
+    private String formatDateTime(LocalDateTime dateTime) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        return dateTime.format(format);
+    }
 }
