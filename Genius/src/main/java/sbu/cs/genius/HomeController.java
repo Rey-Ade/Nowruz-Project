aef7ca45aae04da8fa76ecc608d04e8452b4e865
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-04-16 17:12:55 +0330

    added MouseEvent to top songs list view

--- Genius/src/main/java/sbu/cs/genius/HomeController.java
+++ Genius/src/main/java/sbu/cs/genius/HomeController.java
@@ -1,6 +1,7 @@
 package sbu.cs.genius;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,6 +9,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import sbu.cs.genius.account.User;
 import sbu.cs.genius.content.Song;
@@ -34,28 +36,56 @@ public class HomeController implements Initializable {
             newAlbumButton.setVisible(false);
             newSingleButton.setVisible(false);
         }
-        // uses is an "Artist"
+        // user is an "Artist"
         else {
             newAlbumButton.setOnAction(this::newAlbum);
             newSingleButton.setOnAction(this::newSingle);
         }
 
         // initialize ListView
-        String[] topSongs = new String[5];
+        ArrayList<Song> topSongs = new ArrayList<>();
+        String[] stringTopSongs = new String[5];
         List<Song> tempAllSongs = new ArrayList<>(Song.getAllSongs());
         Song topSong = Song.getAllSongs().getFirst();
+        // find top 5 songs
         for (int j = 0; j < 5; j++) {
             for (Song song : tempAllSongs) {
                 if (song.getViewsCount() > topSong.getViewsCount()) {
                     topSong = song;
                 }
             }
-            topSongs[j] = topSong.toString();
+            topSongs.add(topSong);
+            stringTopSongs[j] = topSong.toString();
             tempAllSongs.remove(topSong);
             topSong = tempAllSongs.getFirst();
         }
-        topSongsListView.getItems().addAll(topSongs);
-        System.out.println("-> top songs ListView is initialized");
+        topSongsListView.getItems().addAll(stringTopSongs);
+        System.out.println("-> top songs ListView is populated");
+
+        topSongsListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                int index = topSongsListView.getSelectionModel().getSelectedIndex();
+                System.out.println("-> selected " + topSongsListView.getSelectionModel().getSelectedItem());
+                // load song controller
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/sbu/cs/genius/song-view.fxml"));
+                Parent root = null;
+                try {
+                    root = loader.load();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                SongController songController = loader.getController();
+                songController.setScene(topSongs.get(index));
+                // switch scenes
+                Stage stage = (Stage) topSongsListView.getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.show();
+                System.out.println("-> switched to song scene");
+            }
+        });
     }
 
     @FXML
